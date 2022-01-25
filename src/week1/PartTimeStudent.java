d77c4f875c2ef73a0018b272060e19b3ecadbcf0
huy5801 <huy5850@gmail.com>
2022-01-25 10:06:59 -0500

    add contructors

---
+++ src/week1/PartTimeStudent.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week1;
+
+/**
+ *
+ * @author chuong5850
+ */
+public class PartTimeStudent extends Student{
+    private int numCourses;
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+}
