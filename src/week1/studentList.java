278090aae97d4e0dfd0cfd397e4fb044dda5da26
huy5801 <huy5850@gmail.com>
2022-01-25 09:21:26 -0500

    add contructors

--- src/week1/studentList.java
+++ src/week1/studentList.java
@@ -9,8 +9,43 @@ package week1;
  * @author chuong5850
  */
 public class studentList {
+    private String studentId;
+    private String studentName;
+    private String address;
+    
+    public studentList(String studentId, String studentName, String address) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.address = address;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
     public static void main(String[] args){
-        Student[] studentList = new Student[3];
+        Student[] studentList = new Student[4];
         studentList[0] = new Student("01","John A");
         studentList[1] = new Student("02","John B");
         studentList[2] = new Student("03","John C");
